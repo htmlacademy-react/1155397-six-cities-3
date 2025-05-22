b6799857b720144b2a245e4cfd18a08518981b7f
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2025-05-22 16:55:06 +0300

    Добавление маршрутизации

---
+++ src/components/private-route.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import { AuthorizationStatus } from '../const';
+
+type PrivateRouteProps = {
+    authorizationStatus: AuthorizationStatus;
+    children: JSX.Element;
+}
+
+function PrivateRoute({authorizationStatus, children}: PrivateRouteProps) {
+  return authorizationStatus === AuthorizationStatus.Auth ? children : <Navigate to={'/login'} />;
+}
+
+export default PrivateRoute;
