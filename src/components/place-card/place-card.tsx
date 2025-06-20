91a345b2584a8e61f7d0107e68f237302236baa6
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2025-06-20 11:06:45 +0300

    Добавление карты и списка ближайших предложений

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -7,65 +7,74 @@ type TPlaceCardVariant = keyof typeof CardVariants;
 
 type PlaceCardProps = {
   offer: TOffer;
-  variant: TPlaceCardVariant;
-  onPlaceCardHover?: () => void;
-  onPlaceCardLeave?: () => void;
+  variant: Extract<TPlaceCardVariant, 'primary'>;
+  onPlaceCardHoverChange: (id: string | null) => void;
+} | {
+  offer: TOffer;
+  variant: Exclude<TPlaceCardVariant, 'primary'>;
+  onPlaceCardHoverChange?: never;
 }
 
-function PlaceCard({offer, variant, onPlaceCardHover, onPlaceCardLeave}: PlaceCardProps) {
+function PlaceCard({offer, variant, onPlaceCardHoverChange}: PlaceCardProps) {
   const {id, title, type, price, isFavorite, isPremium, rating, images} = offer;
   const {prefix, width, height} = CardVariants[variant];
   const favoriteClass = isFavorite ? 'place-card__bookmark-button--active button' : '';
   const [previewImage] = images;
 
+  const handleMouseEnter = onPlaceCardHoverChange && variant === 'primary'
+    ? () => onPlaceCardHoverChange(id)
+    : undefined;
+
+  const handleMouseLeave = onPlaceCardHoverChange && variant === 'primary'
+    ? () => onPlaceCardHoverChange(null)
+    : undefined;
+
   return (
-    <Link to={`/offer/${id}`}>
-      <article
-        className={`${prefix}__card place-card`}
-        onMouseEnter={onPlaceCardHover}
-        onMouseLeave={onPlaceCardLeave}
-      >
-        {isPremium &&
+    <article
+      className={`${prefix}__card place-card`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {isPremium &&
         <div className="place-card__mark">
           <span>Premium</span>
         </div>}
-        <div className={`${prefix}__image-wrapper place-card__image-wrapper`}>
-          <a href="#">
-            <img
-              className="place-card__image"
-              src={previewImage}
-              width={width}
-              height={height}
-              alt="Place image"
-            />
-          </a>
-        </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button className={`place-card__bookmark-button ${favoriteClass} button`} type="button">
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">In bookmarks</span>
-            </button>
+      <div className={`${prefix}__image-wrapper place-card__image-wrapper`}>
+        <Link to={`/offer/${id}`}>
+          <img
+            className="place-card__image"
+            src={previewImage}
+            width={width}
+            height={height}
+            alt="Place image"
+          />
+        </Link>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{ width: calculateStarRating(rating) }}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            {title}
-          </h2>
-          <p className="place-card__type">{type}</p>
+          <button className={`place-card__bookmark-button ${favoriteClass} button`} type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
         </div>
-      </article>
-    </Link>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: calculateStarRating(rating) }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          {title}
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
   );
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,32 @@
 import dayjs from 'dayjs';
+import offers from './mocks/offers';
 import { TReviews } from './types/reviews';
+import { TOffer, TOffers } from './types/offers';
 
+const MAX_NEAR_OFFERS = 3;
 
 export const calculateStarRating = (rating: number) => `${ Math.round(rating) * 100 / 5}%`;
 
 export const formatDateReview = (date: string) => dayjs(date).format('MMMM YYYY');
-
-// export const sortReviews = (arr: string[], dateProperty:string) => arr.sort((a, b) => {
-//   const dateA = dayjs(a[dateProperty]);
-//   const dateB = dayjs(b[dateProperty]);
-//   return dateB - dateA;
-// });
 
 export const sortReviewsByDate = (reviews: TReviews | null) => {
   if (reviews && reviews.length > 1) {
     reviews.sort((firstReview, secondReview) => dayjs(secondReview.date).valueOf() - dayjs(firstReview.date).valueOf());
     return reviews;
   }
+};
+
+export const getNearOffers = (offer: TOffer) => {
+  const nearOffers: TOffers = [];
+
+  for(let i = 0; i < offers.length; i++) {
+    if(offers[i].id !== offer.id && offers[i].city.name === offer.city.name) {
+      nearOffers.push(offers[i]);
+    }
+
+    if(nearOffers.length >= MAX_NEAR_OFFERS) {
+      break;
+    }
+  }
+  return nearOffers;
 };
